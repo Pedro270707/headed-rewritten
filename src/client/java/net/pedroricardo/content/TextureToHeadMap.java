7fec3017288c36a8654f79cf52af79b251f053d0
Pedro <pedro2007.prrr@gmail.com>
2024-01-27 17:52:11 -0300

    Texture replaced with render layer
    (cherry picked from commit 3f520f907727379bbed65546343e9237e10228dd)

--- src/client/java/net/pedroricardo/content/TextureToHeadMap.java
+++ src/client/java/net/pedroricardo/content/TextureToHeadMap.java
@@ -1,8 +1,8 @@
 package net.pedroricardo.content;
 
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.block.entity.SkullBlockEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLoader;
-import net.minecraft.util.Identifier;
 import net.pedroricardo.content.features.HeadedFeatureRenderer;
 
 import java.util.*;
@@ -11,21 +11,21 @@ import java.util.function.Function;
 public class TextureToHeadMap {
     public static final LinkedHashMap<String, HeadedHead> MAP = new LinkedHashMap<>();
 
-    public static HeadedHead addHead(String originalTexture, Function<EntityModelLoader, SkullBlockEntityModel> modelFunction, Identifier modelTexture, String name, String noteBlockSound) {
-        return MAP.put(originalTexture, new HeadedHead(originalTexture, modelFunction, modelTexture, name, noteBlockSound));
+    public static HeadedHead addHead(String originalTexture, Function<EntityModelLoader, SkullBlockEntityModel> modelFunction, RenderLayer renderLayer, String name, String noteBlockSound) {
+        return MAP.put(originalTexture, new HeadedHead(originalTexture, modelFunction, renderLayer, name, noteBlockSound));
     }
 
     @SafeVarargs
-    public static HeadedHead addHead(String originalTexture, Function<EntityModelLoader, SkullBlockEntityModel> modelFunction, Identifier modelTexture, String name, String noteBlockSound, Function<EntityModelLoader, HeadedFeatureRenderer>... featureRenderers) {
-        return MAP.put(originalTexture, new HeadedHead(originalTexture, modelFunction, modelTexture, name, noteBlockSound, Arrays.asList(featureRenderers)));
+    public static HeadedHead addHead(String originalTexture, Function<EntityModelLoader, SkullBlockEntityModel> modelFunction, RenderLayer renderLayer, String name, String noteBlockSound, Function<EntityModelLoader, HeadedFeatureRenderer>... featureRenderers) {
+        return MAP.put(originalTexture, new HeadedHead(originalTexture, modelFunction, renderLayer, name, noteBlockSound, Arrays.asList(featureRenderers)));
     }
 
-    public static HeadedHead addHeadNotInGroup(String originalTexture, Function<EntityModelLoader, SkullBlockEntityModel> modelFunction, Identifier modelTexture, String name, String noteBlockSound) {
-        return MAP.put(originalTexture, new HeadedHead(originalTexture, modelFunction, modelTexture, name, noteBlockSound, false));
+    public static HeadedHead addHeadNotInGroup(String originalTexture, Function<EntityModelLoader, SkullBlockEntityModel> modelFunction, RenderLayer renderLayer, String name, String noteBlockSound) {
+        return MAP.put(originalTexture, new HeadedHead(originalTexture, modelFunction, renderLayer, name, noteBlockSound, false));
     }
 
     @SafeVarargs
-    public static HeadedHead addHeadNotInGroup(String originalTexture, Function<EntityModelLoader, SkullBlockEntityModel> modelFunction, Identifier modelTexture, String name, String noteBlockSound, Function<EntityModelLoader, HeadedFeatureRenderer>... featureRenderers) {
-        return MAP.put(originalTexture, new HeadedHead(originalTexture, modelFunction, modelTexture, name, noteBlockSound, Arrays.asList(featureRenderers), false));
+    public static HeadedHead addHeadNotInGroup(String originalTexture, Function<EntityModelLoader, SkullBlockEntityModel> modelFunction, RenderLayer renderLayer, String name, String noteBlockSound, Function<EntityModelLoader, HeadedFeatureRenderer>... featureRenderers) {
+        return MAP.put(originalTexture, new HeadedHead(originalTexture, modelFunction, renderLayer, name, noteBlockSound, Arrays.asList(featureRenderers), false));
     }
 }
